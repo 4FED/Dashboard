a19918c6a0723e1156f16216d866c34c8a862d86
arthur van schravendijk <arthur_vanschravendijk@hotmail.com>
2014-10-20 12:59:53 +0200

    new api calls for getting and registering users

--- api/index.php
+++ api/index.php
@@ -29,8 +29,54 @@ class apiCalls
 
 	function registerUser($initials, $surname, $name, $email, $password) {
 
-	    $statement = $this->pdo->prepare("INSERT INTO `user` (initials, surname, name, email, password) 
-	    									VALUES ('".$initials."', '".$surname."', '".$name."', '".$email."', '".$password."');");
+		try {		
+		    $statement = $this->pdo->prepare("INSERT INTO `user` (initials, surname, name, email, password) 
+		    									VALUES (:initials, :surname, :name, :email, :password);");
+		    $result = !$statement->execute(array(
+		    	':initials' => $initials,
+		    	':surname' => $surname,
+		    	':name' => $name,	 
+		    	':email' => $email,
+		    	':password' => $password
+		    ));
+		} catch(PDOException $e) {
+			echo 'Error: ' . $e->getMessage();
+	    }
+	    return  $this->pdo->lastInsertId();
+	}
+	
+	function registerExercise($userID, $title, $responsePrevention, $category, $fearFactor) {
+		switch ($category) {
+			case 0:
+				$category = "control";
+				break;
+			case 1:
+				$category = "wash";
+				break;
+			case 2:
+				$category = "normal";
+				break;
+		}
+
+		try {		
+		    $statement = $this->pdo->prepare("INSERT INTO `exercise` (userID, title, responsePrevention, category, fearFactor) 
+		    									VALUES (:userID, :title, :responsePrevention, :category, :fearFactor);");
+		    $result = $statement->execute(array(
+		    	':title' => $title,
+		    	':userID' => $userID,
+		    	':responsePrevention' => $responsePrevention,
+		    	':category' => $category,	 
+		    	':fearFactor' => $fearFactor
+		    ));
+		} catch(PDOException $e) {
+			echo 'Error: ' . $e->getMessage();
+	    }
+	    return  $this->pdo->lastInsertId();
+	}
+
+	function getExercise($userID)	{
+			
+		 $statement = $this->pdo->prepare("SELECT * FROM `exercise` WHERE userID= '".$userID."'");
 	    if (!$statement->execute()) {
 	    	print_r("Error in query ".$this->pdo>errorInfo());
 	    	break;
@@ -54,9 +100,11 @@ switch ($_GET['function']) {
 	case 'registerUser':
 		print_r($apiCalls->registerUser($_GET['initials'], $_GET['surname'], $_GET['name'],$_GET['email'],  $_GET['password']));
 		break;
-	
-	default:
-		# code...
+	case 'registerExercise':
+		print_r($apiCalls->registerExercise($_GET['userID'], $_GET['title'], $_GET['responsePrevention'], $_GET['category'],$_GET['fearFactor']));
+		break;
+	case 'getExercise':
+		print_r($apiCalls->getExercise($_GET['userID']));
 		break;
 }
  ?>
