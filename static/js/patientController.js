e73ee09bea83d3d3a202156f9923f15ff718a211
arthur van schravendijk <arthur_vanschravendijk@hotmail.com>
2014-11-15 15:49:14 +0100

    basic setup patient page 0.1

---
+++ static/js/patientController.js
@@ -0,0 +1,46 @@
+var ocdDashboard = ocdDashboard || {};
+
+(function () {
+	Parse.initialize("e2pCNYD20d5ynvUPKyUud5G20evDRI4pmHiqrvPw", "6IEcqXamMkjJsssIaFPiTDKH1azNB6wOtR5kuAIP");
+
+	var sections = {
+		home: function (){
+			Transparency.render(myFunctions.getOneEl(".patient"), ocdDashboard.Patient.content, ocdDashboard.Patient.directives);
+		},
+		toggle: function (show, hide) {
+			var show = myFunctions.getOneEl("." + show);
+			var hide =  myFunctions.getAllEl("." + hide);
+			for (var i = 0; i < hide.length; i++) {
+				hide[i].classList.remove('active');
+			};
+
+			show.classList.add('active');				
+		} 
+	};
+
+	ocdDashboard.router = {
+		init: function () {
+			var reroute = "http://localhost/4fed/Dashboard/#user/login"
+			routie({
+	    		':id': function() {	
+	    			if (Parse.User.current()) {
+	    				// SHOTGUN.listen("getPatienten", sections.home);
+	    				// ocdDashboard.Patient.get();
+	    				sections.toggle("home", "content");
+	    			}else{
+	    				window.location.replace(reroute);
+	    			};
+	    		},
+	    		'': function(){
+	    			if (Parse.User.current()) {
+	    				window.location.replace("http://localhost/4fed/Dashboard/home.html#home");
+	    			}else{
+	    				window.location.replace(reroute);
+	    			};
+	    		}
+			});
+		} 
+	};
+	ocdDashboard.router.init();
+	myFunctions.AddClickEvent(".eventButton");	
+})();
